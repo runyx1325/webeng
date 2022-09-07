5a45d2a7d3a6919a2cc385fc8c9a209a34c0e720
Luca <lucaburst11@gmail.com>
2022-09-07 17:49:18 +0200

    nothing

--- cart/index.php
+++ cart/index.php
@@ -19,8 +19,67 @@
     </div>
     <div class="content" id="content">
         <h1><b>Shopping</b></h1>
+        <div class="container">
+            <img src="../resources/pixelartGluecksklee.png" alt="Klee" />
+            <div class="overlay"></div>
+            <div class="button" onclick=addCart()> BUY </a></div>
+        </div>
     </div>
     <div class="footer position-absolute bottom-0 d-flex container-fluid" style="background-color: #b3b3b3; height: 100px;" >
     </div>
 </body>
 </html>
+<style>
+    .container {
+    position: relative;
+    margin-top: 50px;
+    width: 500px;
+    height: 300px;
+    }
+    .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0);
+    transition: background 0.5s ease;
+    }
+
+    .container:hover .overlay {
+    display: block;
+    background: rgba(0, 0, 0, .3);
+    }
+
+
+    img {
+    /*position: absolute;
+    /*width: 500px;
+    height: 300px;*/
+    left: 0;
+    }
+
+
+    .button {
+    position: absolute  ;
+    width: 500px;
+    left:0;
+    top: 180px;
+    text-align: center;
+    opacity: 0;
+    transition: opacity .35s ease;
+    }
+
+    .button a {
+    width: 200px;
+    padding: 12px 48px;
+    text-align: center;
+    color: white;
+    border: solid 2px white;
+    z-index: 1;
+    }
+
+    .container:hover .button {
+    opacity: 1;
+    }
+</style>
